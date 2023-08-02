779a2fc33d38fc9fc2b572e7abe6f10869fa5ac9
Vyacheslav Kalin <provak.gg@gmail.com>
2023-08-02 18:25:48 +0600

    added site with styles, the project is configured under the MVC pattern

--- SkillProfiWeb/Program.cs
+++ SkillProfiWeb/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+
+app.UseRouting();
+app.MapControllerRoute("default", "{controller=Hero}/{action=Index}/{id?}");
 
 app.Run();
