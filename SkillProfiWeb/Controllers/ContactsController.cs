b79da13a0060f4157c0ca16315c0480b40605ac5
Vyacheslav Kalin <provak.gg@gmail.com>
2023-08-09 07:52:00 +0600

    added model SiteConfig for Web and WebAPI projects, add contollers Contacts for Web and WebAPI projects, added view Contacts in Web project, corrected Title in pages

---
+++ SkillProfiWeb/Controllers/ContactsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SkillProfiWeb.Models;
+
+namespace SkillProfiWeb.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            ViewData["Title"] = "SkillProfi - Контакты";
+            var config = await SiteConfig.GetInstance();
+            return View(config);
+        }
+    }
+}
---
+++ SkillProfiWebApi/Controllers/ConfigController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SkillProfiWebApi.Models;
+
+namespace SkillProfiWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+        // GET: api/<ConfigController>
+        [HttpGet]
+        public string Get()
+        {
+            return JsonConvert.SerializeObject(SiteConfig.GetInstance());
+        }
+    }
+}
