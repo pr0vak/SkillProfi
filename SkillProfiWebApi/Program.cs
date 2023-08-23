1811124657b3152a65b17fb99deb234636ed28e3
Vyacheslav Kalin <provak.gg@gmail.com>
2023-08-23 09:28:23 +0600

    updated README, deleted swagger in Web API project

--- SkillProfiWebApi/Program.cs
+++ SkillProfiWebApi/Program.cs
@@ -36,15 +36,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
